7a2e64e02c54c91e9883edf0a53de7c7df9f0499
Ilia Kulakov <ilia_kulakov@epam.com>
2019-10-14 09:47:59 +0300

    Fix bug in test

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -172,7 +172,7 @@ public class OwnerControllerTests {
             .andExpect(model().attribute("owner", hasProperty("address", is("110 W. Liberty St."))))
             .andExpect(model().attribute("owner", hasProperty("city", is("Madison"))))
             .andExpect(model().attribute("owner", hasProperty("telephone", is("6085551023"))))
-            .andExpect(view().name("owners/createOrUpdateOwnerFormm"));
+            .andExpect(view().name("owners/createOrUpdateOwnerForm"));
     }
 
     @Test
